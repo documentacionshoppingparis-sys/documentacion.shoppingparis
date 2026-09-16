8504d095a8343904b4896abff38148767dff243a
documentacionshoppingparis-sys <documentacion.shoppingparis@gmail.com>
2026-09-16 10:10:38 -0300

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,19 @@
-// Reemplazar por la configuración REAL obtenida en Firebase Console.
+// Configuración Firebase - docshoppingparis
+// Este proyecto utiliza Firebase SDK compat cargado desde index.html,
+// por lo que NO se usan imports ES Modules en este archivo.
+
 const firebaseConfig = {
-  apiKey: "REEMPLAZAR",
-  authDomain: "REEMPLAZAR.firebaseapp.com",
-  projectId: "REEMPLAZAR",
-  storageBucket: "REEMPLAZAR.appspot.com",
-  messagingSenderId: "REEMPLAZAR",
-  appId: "REEMPLAZAR"
+  apiKey: "AIzaSyCrmF2oDA4ZQeSqvkuHcRDOQUJQpAu28Fs",
+  authDomain: "docshoppingparis.firebaseapp.com",
+  projectId: "docshoppingparis",
+  storageBucket: "docshoppingparis.firebasestorage.app",
+  messagingSenderId: "401673194386",
+  appId: "1:401673194386:web:fad2464f43437441884f4b",
+  measurementId: "G-HVJ549FXT2"
 };
 
+// Inicialización utilizada por toda la aplicación.
 firebase.initializeApp(firebaseConfig);
+
 const auth = firebase.auth();
 const db = firebase.firestore();
